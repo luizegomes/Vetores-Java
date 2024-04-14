4573a3a85ec696475e4a52358e2f746570810d9c
Luize Bernardo <luizegomes@live.com>
2024-04-14 18:41:22 -0300

    Create Vetores.java

---
+++ src/vetores/Vetores.java
@@ -0,0 +1,30 @@
+
+package vetores;
+
+import java.util.Scanner;
+
+
+public class Vetores {
+
+    //Criem um programa que peça 10 números, armazene eles em um vetor e diga qual elemento é o maior, qual é o menor.
+    public static void main(String[] args) {
+        int num[] = new int[10];
+        int maior = num[0];
+        int menor = Integer.MAX_VALUE;
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite 10 números: ");
+        for (int i=0; i<num.length; i++){
+            System.out.println("Número "+(i+1)+ ": ");
+            num[i]=leitor.nextInt();
+        } for (int i=0; i<num.length;i++){
+        if(num[i]>maior){
+            maior=num[i];
+        }if (num[i]<menor){
+            menor=num[i];
+        }
+    }
+        System.out.println("Maior número é: "+maior);
+        System.out.println("Menor número é: "+menor);
+    }
+    
+}
